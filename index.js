424cbb1327b91477237e330102ade5d693ba7182
Ray Raven <rayzafir@aol.com>
2020-12-14 04:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var name = "Joe"
+var name = "Susan"
+if (name === "Susan") {
+  console.log ("The name is correct")
+} else {
+  console.log ("Expected the name to be Susan")
+}
 var height = "74"
 
 
